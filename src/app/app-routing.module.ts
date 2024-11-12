58b58bf876d9e668498ce80f00aaa3c958ef057c
Lakshay <garglakshay2002@gmail.com>
2024-11-12 22:17:42 +0530

    sip and lumpsum

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SipCalculatorComponent } from './sip-calculator/sip-calculator.component';
+import { LumpsumCalculatorComponent } from './lumpsum-calculator/lumpsum-calculator.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'sip-calculator',component:SipCalculatorComponent},
+  {path:'lumpsum-calculator',component:LumpsumCalculatorComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SipCalculatorComponent } from './sip-calculator/sip-calculator.component';
+import { LumpsumCalculatorComponent } from './lumpsum-calculator/lumpsum-calculator.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SipCalculatorComponent,
+    LumpsumCalculatorComponent
   ],
   imports: [
     BrowserModule,
